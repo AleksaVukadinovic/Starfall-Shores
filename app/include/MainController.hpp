e38a95bd8e4ce16d6281ae51c4546c7373dd24f3
Aleksa Vukadinovic <alexvukadin8@gmail.com>
2025-06-05 14:05:28 +0200

    move the implementation of set_common_shader_variables from .hpp to .cpp file

--- app/include/MainController.hpp
+++ app/include/MainController.hpp
@@ -65,30 +65,13 @@ namespace app {
 
         void update_camera();
 
-        bool m_is_day = true;
-        std::string active_daytime_skybox = "skybox_day";
+        bool m_is_day                       = true;
+        std::string active_daytime_skybox   = "skybox_day";
         std::string active_nighttime_skybox = "skybox_night";
 
         void set_common_shader_variables(const engine::resources::Shader *shader,
                                          const engine::graphics::Camera *camera,
-                                         const engine::graphics::GraphicsController *graphics) const {
-            const auto light_position = m_is_day ? glm::vec3(0.0f, 60.0f, 0.0f) : glm::vec3(12.0f, 25.0f, 6.0f);
-            const auto ambient = m_is_day ? glm::vec3(0.2f) : glm::vec3(0.1f);
-            const auto diffuse = m_is_day ? glm::vec3(0.5f) : glm::vec3(0.3f);
-            const auto specular = m_is_day ? glm::vec3(0.1) : glm::vec3(0.05f);
-            const float shininess = m_is_day ? 1024.0f : 2048.0f;
-            const auto light_color = m_is_day ? glm::vec3(1.0f, 1.0f, 1.0f) : glm::vec3(1.0f, 0.7f, 0.1f);
-            shader->use();
-            shader->set_vec3("light.position", light_position);
-            shader->set_vec3("light.ambient", ambient);
-            shader->set_vec3("light.diffuse", diffuse);
-            shader->set_vec3("light.specular", specular);
-            shader->set_vec3("lightColor", light_color);
-            shader->set_float("material.shininess", shininess);
-            shader->set_vec3("viewPos", camera->Position);
-            shader->set_mat4("projection", graphics->projection_matrix());
-            shader->set_mat4("view", camera->view_matrix());
-        }
+                                         const engine::graphics::GraphicsController *graphics) const;
     };
 }
 
--- app/src/MainController.cpp
+++ app/src/MainController.cpp
@@ -65,6 +65,27 @@ namespace app {
         get<engine::platform::PlatformController>()->swap_buffers();
     }
 
+    void MainController::set_common_shader_variables(const engine::resources::Shader *shader,
+                                                     const engine::graphics::Camera *camera,
+                                                     const engine::graphics::GraphicsController *graphics) const {
+        const auto light_position = m_is_day ? glm::vec3(0.0f, 60.0f, 0.0f) : glm::vec3(12.0f, 25.0f, 6.0f);
+        const auto ambient        = m_is_day ? glm::vec3(0.2f) : glm::vec3(0.1f);
+        const auto diffuse        = m_is_day ? glm::vec3(0.5f) : glm::vec3(0.3f);
+        const auto specular       = m_is_day ? glm::vec3(0.1) : glm::vec3(0.05f);
+        const float shininess     = m_is_day ? 1024.0f : 2048.0f;
+        const auto light_color    = m_is_day ? glm::vec3(1.0f, 1.0f, 1.0f) : glm::vec3(1.0f, 0.7f, 0.1f);
+        shader->use();
+        shader->set_vec3("light.position", light_position);
+        shader->set_vec3("light.ambient", ambient);
+        shader->set_vec3("light.diffuse", diffuse);
+        shader->set_vec3("light.specular", specular);
+        shader->set_vec3("lightColor", light_color);
+        shader->set_float("material.shininess", shininess);
+        shader->set_vec3("viewPos", camera->Position);
+        shader->set_mat4("projection", graphics->projection_matrix());
+        shader->set_mat4("view", camera->view_matrix());
+    }
+
     void MainController::draw() {
         bloom_controller->prepare_hdr();
         draw_water();
